ee4f4b743e86590b6ceff54ecbec3228c6b46161
alison626k <alison.ctk@gmail.com>
2021-04-15 23:15:21 +0800

    waffle

---
+++ build/precache-manifest.ca9ba3c11e9a438c9a96174d1304a259.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d03cad424cae2179e5beae69d250dc",
+    "url": "/MIMS21-GEAR/index.html"
+  },
+  {
+    "revision": "451a8547585d574d86cf",
+    "url": "/MIMS21-GEAR/static/css/main.927049d3.chunk.css"
+  },
+  {
+    "revision": "72232cf0611b0bed2135",
+    "url": "/MIMS21-GEAR/static/js/2.32b0df9c.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/MIMS21-GEAR/static/js/2.32b0df9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451a8547585d574d86cf",
+    "url": "/MIMS21-GEAR/static/js/main.77f1c96c.chunk.js"
+  },
+  {
+    "revision": "5ac330c7e1fbb8af2224",
+    "url": "/MIMS21-GEAR/static/js/runtime-main.7fa28056.js"
+  },
+  {
+    "revision": "376336f20b160dc13251d7bd0c399969",
+    "url": "/MIMS21-GEAR/static/media/Brand.376336f2.svg"
+  },
+  {
+    "revision": "a7523d00721144939921eab202a1a608",
+    "url": "/MIMS21-GEAR/static/media/Housetype.a7523d00.svg"
+  },
+  {
+    "revision": "d5a648c1c68e286015582b9bf76d3091",
+    "url": "/MIMS21-GEAR/static/media/Moneybag.d5a648c1.svg"
+  },
+  {
+    "revision": "d9d81735d1822a57f5353dd8ba081691",
+    "url": "/MIMS21-GEAR/static/media/carBackgroundClear.d9d81735.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/MIMS21-GEAR/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/MIMS21-GEAR/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/MIMS21-GEAR/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/MIMS21-GEAR/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/MIMS21-GEAR/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/MIMS21-GEAR/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/MIMS21-GEAR/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/MIMS21-GEAR/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/MIMS21-GEAR/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/MIMS21-GEAR/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/MIMS21-GEAR/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "445ae77b5671f5b41021ca8f6cd93f59",
+    "url": "/MIMS21-GEAR/static/media/logo.445ae77b.png"
+  }
+]);

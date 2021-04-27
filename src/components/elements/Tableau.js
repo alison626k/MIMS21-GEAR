a4da85aebb2006a8cc1c8d33b5b385a34c50aa95
alison626k <alison.ctk@gmail.com>
2021-04-27 05:01:14 -0700

    chart

--- src/components/elements/Tableau.js
+++ src/components/elements/Tableau.js
@@ -1,37 +1,11 @@
 import React, { Component } from 'react';
-import tableau from 'tableau-api';
- 
- 
-class Test extends Component {
-    componentDidMount() {
-      this.initViz();
-    }
-  
-    //Function call API
-    initViz() {
-      const vizUrl =
-        "https://prod-apnortheast-a.online.tableau.com/t/alison/views/GEAR_EDA/brandcharger?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link";
-      const options = {
-        height: "100vh",
-        width: "100%",
-        hideTabs: false,
-        hideToolbar: true,
-        backgroundColor: '#151719'
-      };
-      const vizContainer = this.vizContainer;
-      let viz = new window.tableau.Viz(vizContainer, vizUrl, options);
-    }
-  
-    render() {
-      return (
-        <div
-          ref={div => {
-            this.vizContainer = div;
-          }}
-        />
-      );
-    }
-  }
- 
- 
+import TableauReport from 'tableau-react';
+
+const Test = props => (
+  <TableauReport
+    url="https://prod-apnortheast-a.online.tableau.com/t/alison/views/GEAR_EDA/brandcharger?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link"
+    token="<TRUSTED TICKET HERE>"
+  />
+)
+
 export default Test;
---
+++ src/components/sections/Equity.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionSplitProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+import EquityCapital from '../elements/EquityCapital';
+import EquityHouse from '../elements/EquityHouse';
+import EquityIncome from '../elements/EquityIncome';
+import EquityJob from '../elements/EquityJob';
+import EquityRace from '../elements/EquityRace';
+import Test from '../elements/Tableau';
+
+
+const propTypes = {
+  ...SectionSplitProps.types
+}
+
+const defaultProps = {
+  ...SectionSplitProps.defaults
+}
+
+const Equity = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-split section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-split-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const splitClasses = classNames(
+    'split-wrap',
+    invertMobile && 'invert-mobile',
+    invertDesktop && 'invert-desktop',
+    alignTop && 'align-top'
+  );
+
+  const sectionHeader = {
+    title: 'EV Charger Equity Gap',
+    paragraph: ' Need to be filled'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                <h3 className="mt-0 mb-12">
+                  Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                  < EquityCapital style={{height: "50"}} />
+              </div>
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item" >
+                <h3 className="mt-0 mb-12">
+                  Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                  < EquityHouse style={{height: 50 }} />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                <h3 className="mt-0 mb-12">
+                  Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                  < EquityIncome style={{height: 50 }} />
+              </div>
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item" >
+                <h3 className="mt-0 mb-12">
+                  Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                  < EquityJob style={{height: "50"}} />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                <h3 className="mt-0 mb-12">
+                  Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                  < Test/>
+              </div>
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item" >
+                <h3 className="mt-0 mb-12">
+                  Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                  
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+Equity.propTypes = propTypes;
+Equity.defaultProps = defaultProps;
+
+export default Equity;

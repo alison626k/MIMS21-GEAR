f2b46b4135b30bd5ea35868d029e4c4175b03557
alison626k <alison.ctk@gmail.com>
2021-04-27 05:01:09 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MIMS21-GEAR/precache-manifest.75a42565dc23da5b0de58e637374e828.js"
+  "/MIMS21-GEAR/precache-manifest.7d4dca23783e74f95d850086c95a04a4.js"
 );
 
 self.addEventListener('message', (event) => {
